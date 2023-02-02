23094943b1421a263f0c287b99b8cd757e5a45ec
kruss84 <sebastiankrusche@rocketmail.com>
2023-02-02 21:14:21 +0100

    reduce patch_size

--- scripts/train_volleyball_stage1.py
+++ scripts/train_volleyball_stage1.py
@@ -9,8 +9,8 @@ cfg.training_stage=1
 cfg.stage1_model_path=''
 cfg.train_backbone=True
 
-cfg.batch_size=8
-cfg.test_batch_size=4
+cfg.batch_size=4
+cfg.test_batch_size=2
 cfg.num_frames=1
 cfg.train_learning_rate=1e-5
 cfg.lr_plan={}
